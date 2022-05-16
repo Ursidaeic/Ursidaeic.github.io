8c8ff1bfa0a18c9fc75f91bc4d85aacd526f9847
Ursidaeic <charlie.c.armstead@gmail.com>
2022-05-16 11:15:11 +0100

    misc

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,16 +36,22 @@
 document.querySelectorAll(".carousel").forEach((carousel) => {
 	const items = carousel.querySelectorAll(".carousel__item");
 	const buttonsHtml = Array.from(items, () => {
-		return `<span class="carousel__button"></span>`;
+		return `<a href="#casestudies"><span class="carousel__button"></span></a>`;
 	});
-
 	carousel.insertAdjacentHTML(
-	"beforeend", 
+	"afterbegin", 
 	`
 		<div class="carousel__nav">
 			${buttonsHtml.join("")}
 		</div>
 	`);
+	carousel.insertAdjacentHTML(
+		"beforeend", 
+		`
+			<div class="carousel__nav">
+				${buttonsHtml.join("")}
+			</div>
+		`);
 
 	
 	const buttons = carousel.querySelectorAll(".carousel__button");
